0c07ee73eb7ef411a7b3d05fbdb9ccb68104553e
Felix Freidman <felixfreidman@gmail.com>
2022-12-05 17:20:39 +0300

    Создал компонент Input начал создавать форму авторизации + Стили

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { Component } from "react";
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from "../../components/UI/Input/Input";
 
 class Auth extends Component {
 
+    loginHandler = () => {
+
+    }
+    signupHandler = () => {
+
+    }
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <Input label="Email" />
+                        <Input label="Password" />
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}>Sign in</Button>
+                        <Button
+                            type="primary"
+                            onClick={this.signupHandler}>Sign up</Button>
+                    </form>
+                </div>
             </div>
         )
     }
